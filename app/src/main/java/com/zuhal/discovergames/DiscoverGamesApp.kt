b8d1be3a5cdbf2ad647d9fd3809b6c17b8945afd
zuhalal <zuhalalimulhadi19@gmail.com>
2023-05-10 00:10:30 +0700

    chores: init main screen

---
+++ app/src/main/java/com/zuhal/discovergames/DiscoverGamesApp.kt
@@ -0,0 +1,54 @@
+package com.zuhal.discovergames
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.zuhal.discovergames.ui.navigation.Screen
+import com.zuhal.discovergames.ui.theme.DiscoverGamesTheme
+
+@Composable
+fun DiscoverGamesApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(Screen.Home.route) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DiscoverGamesTheme {
+        Greeting("Android")
+    }
+}
