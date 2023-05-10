f164e852935a60d01ff57e8abc0a682d16c4dddb
zuhalal <zuhalalimulhadi19@gmail.com>
2023-05-11 06:12:37 +0700

    feat: add genre in detail screen

--- app/src/main/java/com/zuhal/discovergames/ui/components/content/DetailContent.kt
+++ app/src/main/java/com/zuhal/discovergames/ui/components/content/DetailContent.kt
@@ -2,6 +2,8 @@ package com.zuhal.discovergames.ui.components.content
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
@@ -16,6 +18,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zuhal.discovergames.R
@@ -44,7 +47,9 @@ fun DetailContent(
             )
         }
         Column(
-            modifier = Modifier.offset(y=(-48).dp).padding(horizontal = 12.dp, vertical = 16.dp),
+            modifier = Modifier
+                .offset(y = (-48).dp)
+                .padding(horizontal = 12.dp, vertical = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(12.dp),
         ) {
@@ -54,6 +59,27 @@ fun DetailContent(
                     fontWeight = FontWeight.Bold
                 )
             )
+            LazyRow(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                itemsIndexed(game.genres, key = { _, genre -> genre.id }) { index, genre ->
+                    Text(
+                        text = genre.name, style = MaterialTheme.typography.subtitle1.copy(
+                            fontWeight = FontWeight.Bold,
+                            textDecoration = TextDecoration.Underline
+                        )
+                    )
+                    if (index != game.genres.size - 1) {
+                        Text(
+                            text=",",
+                            style = MaterialTheme.typography.subtitle1.copy(
+                                fontWeight=FontWeight.Bold
+                            )
+                        )
+                    }
+                }
+            }
             ExpandableText(
                 text = game.description,
                 maxLines = 3
