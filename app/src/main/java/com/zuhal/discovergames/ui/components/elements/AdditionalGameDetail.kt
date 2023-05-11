31c8b528f9ec7a111770814371b6c435a2150acf
zuhalal <zuhalalimulhadi19@gmail.com>
2023-05-11 07:25:53 +0700

    fix: spacing in addtional game detail

--- app/src/main/java/com/zuhal/discovergames/ui/components/elements/AdditionalGameDetail.kt
+++ app/src/main/java/com/zuhal/discovergames/ui/components/elements/AdditionalGameDetail.kt
@@ -1,10 +1,12 @@
 package com.zuhal.discovergames.ui.components.elements
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.zuhal.discovergames.R
 import com.zuhal.discovergames.withDateFormat
 
@@ -16,18 +18,35 @@ fun AdditionalGameDetail(
     esrbRating: String,
     modifier: Modifier = Modifier
 ) {
-    LazyVerticalGrid(columns = GridCells.Fixed(2), modifier = modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier,
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
         item {
-            AdditionalGameDetailSection(title = stringResource(R.string.release_date), content = released.withDateFormat() )
+            AdditionalGameDetailSection(
+                title = stringResource(R.string.release_date),
+                content = released.withDateFormat()
+            )
         }
         item {
-            AdditionalGameDetailSection(title = stringResource(R.string.rating), content = rating.toString() )
+            AdditionalGameDetailSection(
+                title = stringResource(R.string.rating),
+                content = rating.toString()
+            )
         }
         item {
-            AdditionalGameDetailSection(title = stringResource(R.string.ratings_count), content = ratingsCount.toString() )
+            AdditionalGameDetailSection(
+                title = stringResource(R.string.ratings_count),
+                content = ratingsCount.toString()
+            )
         }
         item {
-            AdditionalGameDetailSection(title = stringResource(R.string.esrb_rating), content = esrbRating )
+            AdditionalGameDetailSection(
+                title = stringResource(R.string.esrb_rating),
+                content = esrbRating
+            )
         }
     }
 }
