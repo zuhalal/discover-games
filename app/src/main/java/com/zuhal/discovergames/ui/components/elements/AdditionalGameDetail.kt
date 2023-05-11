0d5de2bad5224cbe20c1c67033d56ef0d2dee921
zuhalal <zuhalalimulhadi19@gmail.com>
2023-05-12 00:38:30 +0700

    chores: add semantics in additional game detail

--- app/src/main/java/com/zuhal/discovergames/ui/components/elements/AdditionalGameDetail.kt
+++ app/src/main/java/com/zuhal/discovergames/ui/components/elements/AdditionalGameDetail.kt
@@ -5,7 +5,10 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import com.zuhal.discovergames.R
 import com.zuhal.discovergames.withDateFormat
@@ -18,6 +21,8 @@ fun AdditionalGameDetail(
     esrbRating: String,
     modifier: Modifier = Modifier
 ) {
+    val context = LocalContext.current
+
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = modifier,
@@ -27,25 +32,37 @@ fun AdditionalGameDetail(
         item {
             AdditionalGameDetailSection(
                 title = stringResource(R.string.release_date),
-                content = released.withDateFormat()
+                content = released.withDateFormat(),
+                modifier = Modifier.semantics(mergeDescendants = true) {
+                    contentDescription = context.getString(R.string.release_date_section)
+                }
             )
         }
         item {
             AdditionalGameDetailSection(
                 title = stringResource(R.string.rating),
-                content = rating.toString()
+                content = rating.toString(),
+                modifier = Modifier.semantics(mergeDescendants = true) {
+                    contentDescription = context.getString(R.string.rating_section)
+                }
             )
         }
         item {
             AdditionalGameDetailSection(
                 title = stringResource(R.string.ratings_count),
-                content = ratingsCount.toString()
+                content = ratingsCount.toString(),
+                modifier = Modifier.semantics(mergeDescendants = true) {
+                    contentDescription = context.getString(R.string.ratings_count_section)
+                }
             )
         }
         item {
             AdditionalGameDetailSection(
                 title = stringResource(R.string.esrb_rating),
-                content = esrbRating
+                content = esrbRating,
+                modifier = Modifier.semantics(mergeDescendants = true) {
+                    contentDescription = context.getString(R.string.esrb_rating_section)
+                }
             )
         }
     }
